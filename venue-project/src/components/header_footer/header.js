2eebf99d4254df65b4f1c497485eb5db55d8d2a9
Zenzi Ali <zeeathome935@gmail.com>
2021-03-23 23:41:29 -0400

    added side drawer with state

--- venue-project/src/components/header_footer/header.js
+++ venue-project/src/components/header_footer/header.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import MenuIcon from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
+import SideDrawer from './SideDrawer';
+
 export default class Header extends Component {
+
+  state = {
+    drawerOpen: false
+  }
   render() {
     return (
       <div>
@@ -21,6 +27,19 @@ export default class Header extends Component {
               <div className="font_righteous header_logo_venue">The Venue</div>
               <div className="font_roboto header_logo_venue">Musical Events</div>
             </div>
+
+            <IconButton
+            aria-label="Menu"
+            color="inherit"
+            onClick={()=> console.log("open")}>
+
+              <MenuIcon />
+
+            </IconButton>
+            <SideDrawer 
+            open={this.state.drawerOpen}
+            onClose={(value)=> this.toggleDrawer(value)}
+            />
           </Toolbar>
         </AppBar>
       </div>
